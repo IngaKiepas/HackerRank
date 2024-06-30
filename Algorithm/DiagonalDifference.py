ceeb71595a2000ac9843ef81c26df6c9f82819eb
inga <kiepas.inga@gmail.com>
2024-06-30 12:53:07 +0200

    Absolute difference between the sums of diagonals

--- Algorithm/DiagonalDifference.py
+++ Algorithm/DiagonalDifference.py
@@ -47,6 +47,7 @@ import os
 import random
 import re
 import sys
+import numpy as np
 
 #
 # Complete the 'diagonalDifference' function below.
@@ -57,8 +58,8 @@ import sys
 
 def diagonalDifference(arr):
     # Write your code here
-    diagonal1 = arr[0][0] + arr[1][1] + arr[2][2]
-    diagonal2 = arr[0][2] + arr[1][1] + arr[2][0]
+    diagonal1 = arr.diagonal()
+    diagonal2 = np.fliplr(arr).diagonal()
     difference = abs(diagonal1-diagonal2)
     return difference
 
